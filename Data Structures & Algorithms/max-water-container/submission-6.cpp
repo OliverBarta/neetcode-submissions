7f29d2330dd65dbe43651012cba7303be86c646a
Oliver Barta <barta3738@gmail.com>
2026-05-23 12:45:48 -0400

    Add: max-water-container - submission-6

---
+++ Data Structures & Algorithms/max-water-container/submission-6.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxArea(vector<int>& heights) {
+        int output = 0;
+        
+        for (int i = 0; i < heights.size(); i++) {
+            for (int j = i + 1; j < heights.size(); j++) {
+                output = max(output, (j - i) * min(heights[i], heights[j]));
+            }
+        }
+        return output;
+    }
+};
